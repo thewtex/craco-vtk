5aaa38c99fb57089ef7d23f6d28c5f47498007d8
Matt McCormick <matt.mccormick@kitware.com>
2019-04-12 14:34:11 -0400

    BUG: Remove vtk.js CSS rules
    For now. Currently resulting in a build error.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const path = require("path");
 const vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.core.rules;
-const cssRules = require('vtk.js/Utilities/config/dependency.js').webpack.css.rules;
 
 module.exports = () => ({
   overrideWebpackConfig: ({ webpackConfig, context: { env } }) => {
-    webpackConfig.module.rules = webpackConfig.module.rules.concat(vtkRules, cssRules)
+    webpackConfig.module.rules = webpackConfig.module.rules.concat(vtkRules)
 
     return webpackConfig;
   }
