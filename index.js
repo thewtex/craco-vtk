0778b459fb240c1163fef07e2024907185105b50
Matt McCormick <matt.mccormick@kitware.com>
2019-04-12 10:43:02 -0400

    BUG: Fix additions of Webpack rules

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require("path");
-const vtkChainWebpack = require("vtk.js/Utilities/config/chainWebpack");
+const vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.core.rules;
+const cssRules = require('vtk.js/Utilities/config/dependency.js').webpack.css.rules;
 
 module.exports = () => ({
   overrideWebpackConfig: ({ webpackConfig, context: { env } }) => {
-    webpackConfig = vtkChainWebpack(webpackConfig);
+    webpackConfig.module.rules = webpackConfig.module.rules.concat(vtkRules, cssRules)
 
     return webpackConfig;
   }
